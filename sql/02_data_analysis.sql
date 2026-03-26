b66d0a8a6b086894f957f995e5d9d5ec3188f2c9
Sharon Maria Francis <sharonmariafrancisart@gmail.com>
2026-03-26 20:45:46 +0530

    Update 02_data_analysis.sql

--- sql/02_data_analysis.sql
+++ sql/02_data_analysis.sql
@@ -1,1 +1,94 @@
+-- Superstore Database Setup
+-- Author: Sharon Maria
+-- Description: Raw table creation for Superstore dataset
 
+
+CREATE DATABASE superstore_db;
+USE superstore_db;
+CREATE TABLE superstore (
+    row_id INT,
+    order_id VARCHAR(50),
+    order_date DATETIME,
+    ship_date DATETIME,
+    ship_mode VARCHAR(50),
+    customer_id VARCHAR(50),
+    customer_name VARCHAR(100),
+    segment VARCHAR(50),
+    country VARCHAR(50),
+    city VARCHAR(50),
+    state VARCHAR(50),
+    postal_code VARCHAR(20),
+    region VARCHAR(50),
+    product_id VARCHAR(50),
+    category VARCHAR(50),
+    sub_category VARCHAR(50),
+    product_name VARCHAR(255),
+    sales DECIMAL(10,2),
+    quantity INT,
+    discount DECIMAL(5,2),
+    profit DECIMAL(10,2)
+);
+
+TRUNCATE TABLE superstore;
+
+LOAD DATA INFILE 'C:\Users\91979\Desktop\Retail-Sales-Customer-Behavior-Analysis\data\Sample_Superstore_cleaned.csv'
+INTO TABLE superstore
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+SHOW VARIABLES LIKE 'secure_file_priv';
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Sample_Superstore_cleaned.csv'
+INTO TABLE superstore
+FIELDS TERMINATED BY '\t'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+TRUNCATE TABLE superstore;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Sample_Superstore_cleaned.csv'
+INTO TABLE superstore
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(
+row_id,
+order_id,
+order_date,
+ship_date,
+ship_mode,
+customer_id,
+customer_name,
+segment,
+country,
+city,
+state,
+postal_code,
+region,
+product_id,
+category,
+sub_category,
+product_name,
+@sales,
+quantity,
+@discount,
+@profit
+)
+SET
+sales = NULLIF(@sales, ''),
+discount = NULLIF(@discount, ''),
+profit = NULLIF(@profit, '');
+
+
+
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Sample_Superstore_cleaned_tab.txt'
+INTO TABLE superstore
+CHARACTER SET latin1
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
